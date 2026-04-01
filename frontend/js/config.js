659d3f77ce146ae1a6d40dabfc2d8fe3762d0ac5
shubhranshu-sahu <shubhranshu2023@gmail.com>
2026-04-02 00:46:28 +0530

    base api

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
-const API_BASE = "http://127.0.0.1:5000/api";
+// const API_BASE = "http://127.0.0.1:5000/api";
 // const API_BASE = "https://campusquery-cf3t.onrender.com/api";
-// const API_BASE = "https://campusquery-dr02.onrender.com/api";
+const API_BASE = "https://campusquery-dr02.onrender.com/api";
